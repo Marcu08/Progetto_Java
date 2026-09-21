96c74b995c670aab9a3f1f5f1d9cad24f95afa14
student <student@itismeucci.com>
2026-09-21 10:52:32 +0200

    Creazione classe

---
+++ src/main/java/com/example/ContoCorrente.java
@@ -0,0 +1,13 @@
+package com.example;
+public class ContoCorrente {
+    private double saldo;
+    private String titolare;
+
+    public IllegalArgumentException (double saldo, String titolare){
+        this.saldo=saldo;
+        this.titolare=titolare;
+        if(saldo<0){
+            
+        }
+    }
+}
